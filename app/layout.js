ef246ac635c4f4a7a708f0896c9e42277f346811
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-01 22:50:07 -0500

    cleanup

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,24 @@ const barlow = Barlow({ weight: ['100', '500', '700', '800', '900'], subsets: ['
 export const metadata = {
   title: 'Tasa de cambio CUP y MLC de Cuba en tiempo real',
   description: 'Obtén la tasa de cambio CUP y MLC de Cuba en tiempo real',
-  image: 'https://cupusd.vercel.app/api/og',
+  keywords: ['Cambio CUP', 'Tasa CUP USD', 'Cambio CUP Cuba'],
+  creator: 'Erich Garcia Cruz',
+  openGraph: {
+    title: 'Tasa de cambio CUP y MLC de Cuba en tiempo real',
+    description: 'Obtén la tasa de cambio CUP y MLC de Cuba en tiempo real',
+    url: 'https://www.cambiocup.com',
+    siteName: 'CAMBIO CUP',
+    images: [
+      {
+        url: 'https://www.cambiocup.com/api/og',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'es_ES',
+    type: 'website',
+  },
+  
 }
 
 export default function RootLayout({ children }) {
