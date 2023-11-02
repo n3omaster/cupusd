7f29e73f21ede5fa4bac6afa3186130d1f554c0a
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-02 09:08:58 -0500

    no cache API fetch

--- app/api/fetch/route.js
+++ app/api/fetch/route.js
@@ -3,12 +3,19 @@ import { saveCoinData } from '../db.js'
 // How to allow only if the request come from the same domain?
 export async function GET(request) {
 
-    // Retirve data from 
-    // https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_CUP and https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_MLC
-    const response = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_CUP')
+    const response = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_CUP', {
+        headers: {
+            'Cache-Control': 'no-cache'
+        }
+    })
     const cupHistory = await response.json()
 
-    const response2 = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_MLC')
+    const response2 = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_MLC', {
+        headers: {
+            'Cache-Control': 'no-cache'
+
+        }
+    })
     const mlcHistory = await response2.json()
 
     // Now save this into DB with prisma
