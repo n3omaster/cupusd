4f5b301c9c91591d9a3218c9795a7b010561d08c
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-08 17:03:25 -0600

    Test API logs

--- app/api/fetch/route.js
+++ app/api/fetch/route.js
@@ -18,8 +18,13 @@ export async function GET(request) {
     })
     const mlcHistory = await response2.json()
 
-    // Now save this into DB with prisma
+    console.log('CUP History Average:', cupHistory.average);
+    console.log('MLC History Average:', mlcHistory.average);
+
     const { cup, mlc } = await saveCoinData(cupHistory.average, mlcHistory.average)
+
+    console.log('Saved CUP Value:', cup);
+    console.log('Saved MLC Value:', mlc);
 
     return Response.json({ cup, mlc })
 }
