eade6fe7530c69dba0f61589174ee47dd4307d02
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-09 23:11:11 -0600

    Remove unused state variable initialized.

--- app/page.js
+++ app/page.js
@@ -10,7 +10,6 @@ export default function Home() {
   const [coin, setCoin] = useState('CUP')
   const [value, setValue] = useState(0)
   const [bgColor, setBgColor] = useState('bg-crimson')
-  const [initialized, setInitialized] = useState(false);
 
   // fetch the value of CUP from DB and populate the value and the color based on the trending from the last 24 hours, cache this value for 5 mins
   useEffect(() => {
