e8668f2703b2d5fc83255631192c87ca15655f77
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2023-11-12 10:29:57 -0600

    Refactor coin data retrieval and update package version.

--- app/api/db.js
+++ app/api/db.js
@@ -3,8 +3,6 @@ import prisma from '../../lib/prisma';
 // Restrieve the last 48 values of exchange where coin is CUP or MLC
 // Find out how cache this for about 5 minutes for every request
 const getCoinData = async () => {
-
-    // Obtén los últimos 48 valores de cambio donde coin es "CUP"
     const cupHistory = await prisma.exchange.findMany({
         where: {
             coinId: 1,
@@ -14,8 +12,6 @@ const getCoinData = async () => {
         },
         take: 48,
     });
-
-    // Obtén los últimos 48 valores de cambio donde coin es "MLC"
     const mlcHistory = await prisma.exchange.findMany({
         where: {
             coinId: 2,
@@ -31,21 +27,18 @@ const getCoinData = async () => {
 
 // Save the coin history into DB
 const saveCoinData = async (cup, mlc) => {
-    
     const rspCup = await prisma.exchange.create({
         data: {
             coinId: 1,
             value: cup,
         },
     });
-    
     const rspMlc = await prisma.exchange.create({
         data: {
             coinId: 2,
             value: mlc,
         },
     });
-
     return { cup, mlc };
 }
 
