83aa5f67c205fe62a2f2f11c52bf86e66cbe0d35
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2024-02-13 22:40:53 -0600

    Refactor API calls and update data insertion

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -5,19 +5,21 @@ export const fetchCache = 'force-no-store'
 export const revalidate = 0
 
 export async function GET() {
+
     const response = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_CUP', {
         headers: { 'Cache-Control': 'no-cache' },
         next: { revalidate: 0 }
     })
     const cupHistory = await response.json()
-
+    
     const response2 = await fetch('https://qvapay.com/api/p2p/completed_pairs_average?coin=BANK_MLC', {
         headers: { 'Cache-Control': 'no-cache' },
         next: { revalidate: 0 }
     })
     const mlcHistory = await response2.json()
-
+    
     const { cup, mlc } = await saveCoinData((cupHistory.average_buy + cupHistory.average_sell) / 2, (mlcHistory.average_buy + mlcHistory.average_sell) / 2)
     const randomNumber = Math.random() * 1000
+
     return NextResponse.json({ cup, mlc, randomNumber })
 }
