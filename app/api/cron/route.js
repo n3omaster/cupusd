3c3195eeae2ab9728f69c0e5fb2106d4f601d405
Erich Garcia Cruz <erichjosegarcia@gmail.com>
2025-11-18 14:04:24 -0600

    Enhance coin data retrieval by adding ETECSA support: Update API routes to include ETECSA history in both GET functions and modify saveCoinData to handle the new coin data, improving overall data coverage.

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -4,18 +4,20 @@ import { saveCoinData } from '@/lib/supabase'
 // Get the average buy and sell prices for the last 24 hours for BANK_CUP and BANK_MLC and CLASICA
 export async function GET() {
 
-    const response = await fetch('https://api.qvapay.com/p2p/completed_pairs_average?coin=BANK_CUP', { headers: { 'Cache-Control': 'no-cache' }, next: { revalidate: 0 } })
-    const cupHistory = await response.json()
+	const response = await fetch('https://api.qvapay.com/p2p/completed_pairs_average?coin=BANK_CUP')
+	const cupHistory = await response.json()
 
-    const response2 = await fetch('https://api.qvapay.com/p2p/completed_pairs_average?coin=BANK_MLC', { headers: { 'Cache-Control': 'no-cache' }, next: { revalidate: 0 } })
-    const mlcHistory = await response2.json()
+	const response2 = await fetch('https://api.qvapay.com/p2p/completed_pairs_average?coin=BANK_MLC')
+	const mlcHistory = await response2.json()
 
-    const response3 = await fetch('https:/api.qvapay.com/p2p/completed_pairs_average?coin=CLASICA', { headers: { 'Cache-Control': 'no-cache' }, next: { revalidate: 0 } })
-    const clasicaHistory = await response3.json()
+	const response3 = await fetch('https:/api.qvapay.com/p2p/completed_pairs_average?coin=CLASICA')
+	const clasicaHistory = await response3.json()
 
-    const { cup, mlc, clasica } = await saveCoinData((cupHistory.average_buy + cupHistory.average_sell) / 2, (mlcHistory.average_buy + mlcHistory.average_sell) / 2, (clasicaHistory.average_buy + clasicaHistory.average_sell) / 2)
+	const response4 = await fetch('https:/api.qvapay.com/p2p/completed_pairs_average?coin=ETECSA')
+	const etecsaHistory = await response4.json()
 
-    const randomNumber = Math.random() * 1000
+	const { cup, mlc, clasica, etecsa } = await saveCoinData((cupHistory.average_buy + cupHistory.average_sell) / 2, (mlcHistory.average_buy + mlcHistory.average_sell) / 2, (clasicaHistory.average_buy + clasicaHistory.average_sell) / 2, (etecsaHistory.average_buy + etecsaHistory.average_sell) / 2)
 
-    return NextResponse.json({ cup, mlc, clasica, randomNumber })
+	const randomNumber = Math.random() * 1000
+	return NextResponse.json({ cup, mlc, clasica, etecsa, randomNumber })
 }
